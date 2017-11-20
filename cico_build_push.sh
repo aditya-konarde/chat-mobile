01178b3d84e57095e2a0d149907b109a7fc6ea7a
Aditya Konarde <aditya.konarde@gmail.com>
2017-11-20 18:59:17 +0530

    Add initial script to build app
    Automate the app build process through script

---
+++ cico_build_push.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Install dependencies for mattermost app
+yum -y update && \
+yum -y install epel-release && \
+yum -y install nodejs zlib.i686 libstdc++.i686 make git android-tools.x86_64 java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless wget unzip && \
+yum clean all
+# Download the android SDK
+cd /opt && \
+wget --output-document=android-sdk.zip --quiet https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip && \
+unzip android-sdk.zip && \
+rm -f android-sdk.zip && \
+mkdir android-sdk-linux && \
+mv tools/ android-sdk-linux/ && \
+chown -R root:root android-sdk-linux
+# Install Yarn
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo &&\
+yum -y install yarn && \
+
+ANDROID_HOME=/opt/android-sdk-linux && \
+GRADLE_HOME=/opt/gradle && \
+PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin && \
+JAVA_HOME=/usr/lib/jvm/jre-openjdk/ && \
+
+# Install Android Platforms
+echo y | sdkmanager "platform-tools" && \
+echo " Installing APIS" && \
+echo y | sdkmanager "add-ons;addon-google_apis-google-23" && \
+echo y | sdkmanager "add-ons;addon-google_apis-google-24" && \
+echo y | sdkmanager "add-ons;addon-google_apis-google-22" && \
+echo y | sdkmanager "add-ons;addon-google_apis-google-21" && \
+echo y | sdkmanager "platforms;android-25" && \
+echo y | sdkmanager "platforms;android-23" && \
+echo y | sdkmanager "build-tools;26.0.1" &&\
+echo y | sdkmanager "build-tools;26.0.0" &&\
+echo y | sdkmanager "build-tools;25.0.2" && \
+echo y | sdkmanager "build-tools;25.0.1" && \
+echo y | sdkmanager "build-tools;25.0.0" && \
+echo y | sdkmanager "build-tools;24.0.0" && \
+echo y | sdkmanager "build-tools;23.0.1" && \
+echo y | sdkmanager "extras;android;m2repository" && \
+echo y | sdkmanager "extras;google;m2repository" && \
+echo y | sdkmanager "extras;google;google_play_services" && \
+mkdir -p $ANDROID_HOME/licenses/ && \
+echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > $ANDROID_HOME/licenses/android-sdk-license && \
+echo "84831b9409646a918e30573bab4c9c91346d8abd" > $ANDROID_HOME/licenses/android-sdk-preview-license;
+echo y | sdkmanager --licenses && \
+
+MATTERMOST_BUILD_VERSION="1.4.1" && \
+cd /opt/ && \
+wget https://github.com/mattermost/mattermost-mobile/archive/v1.4.1.tar.gz && \
+tar xvf v$MATTERMOST_BUILD_VERSION.tar.gz;
+
+# Finally build the app
+cd /opt/mattermost-mobile-$MATTERMOST_BUILD_VERSION && \
+yarn install && \
+make
